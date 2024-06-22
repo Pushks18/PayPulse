3ee95b2bc94c354b852a5d88df250fbed34035a0
Pushkaraj2024 <2020.pushkaraj.baradkar@ves.ac.in>
2024-06-23 01:11:04 +0530

    added landing page

--- Frontend/src/Pages/Landing.jsx
+++ Frontend/src/Pages/Landing.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import Button from "../Components/Button";
 import { useNavigate } from "react-router-dom";
+import Heading from "../Components/Heading";
 
 const Landing = () => {
   const navigate = useNavigate();
   return (
-    <div>
-      <Button onClick={navigate("/signup")} button={"Sign Up"} />
+    <div className="flex flex-col items-center justify-center">
+      <div className="">
+        <Heading text={"Pay Pulse"} />
+      </div>
+      <div className="mt-20">
+        <Button onClick={() => navigate("/signup")} button={"Get Started"} />
+      </div>
     </div>
   );
 };
