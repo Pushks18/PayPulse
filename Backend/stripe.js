9a59f7cf35ce5ed5a2682af6aa615c38049db6c8
Pushks18 <pushks2015@gmail.com>
2025-01-28 16:36:29 -0800

    stripe issue

--- Backend/stripe.js
+++ Backend/stripe.js
@@ -2,5 +2,6 @@
 require("dotenv").config();
 const Stripe = require("stripe");
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+console.log("Stripe secret key:", process.env.STRIPE_SECRET_KEY);
 
 module.exports = stripe;
